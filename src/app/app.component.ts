9c895ec02c559a5e40e65ec6b51bdee760621171
TheCodeBite <kamh.1996.hmak@gmail.com>
2019-11-14 15:49:31 -0600

    proyecto terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'integracionContinua';
+  
+  numero2 = 0;
+  numero1 = 0;
+  resultado = ""
+  sumar(){
+    console.log("numeros: " + this.numero1 + " + " + this.numero2);
+    this.resultado = "resultado es " + (this.numero1 + this.numero2)
+  }
 }
